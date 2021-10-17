3352c0bf3d62983e45790e2130ec933fc7b8f915
bayu <litbang.bungamayang@gmail.com>
2021-10-18 00:48:27 +0700

    - Ganti judul

--- app/Views/Templates/app_layout.php
+++ app/Views/Templates/app_layout.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="UTF-8">
 	<?= $this->renderSection('header') ?>
-  <title>Dashboard BCN</title>
+  <title>Electronic Budgeting Administration</title>
   <meta name="description" content="Dashboard monitoring kinerja pabrik gula">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" type="image/png" href="<?php echo base_url('public/assets/favicon.ico');?>"/>
